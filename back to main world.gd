5728afc56792fcdde444e8cca57ea7abe81607db
Mayank <mayankjod016@gmail.com>
2025-03-28 00:57:56 +0530

    added end scene

--- back to main world.gd
+++ back to main world.gd
@@ -1,6 +1,11 @@
 extends Area3D
 
+func _ready():
+	# Verify setup
+	print("Coin ready! Layers:", collision_layer, " Mask:", collision_mask)
+	body_entered.connect(_on_coin_entered)
 
-func _on_body_entered(body):
-	if body is CharacterBody3D:
+func _on_coin_entered(body):
+	print("Entered:", body.name)
+	if body.is_in_group("player"):
 		get_tree().change_scene_to_file("res://scenes/retro_scene-3.tscn")
